fc54d36a1e61c72b8f4233637707ae366b55a1d6
Dongwoo Yang <ckck803@gmail.com>
2020-12-23 02:44:41 +0900

    Hello Bean 추가

--- src/main/java/com/example/restfulwebservice/controller/HelloWorldController.java
+++ src/main/java/com/example/restfulwebservice/controller/HelloWorldController.java
@@ -1,5 +1,6 @@
 package com.example.restfulwebservice.controller;
 
+import com.example.restfulwebservice.HelloWorldBean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +10,10 @@ public class HelloWorldController {
     @GetMapping("/hello-world")
     public String Hello(){
         return "Hello World";
+    }
+
+    @GetMapping("/hello-world-bean")
+    public HelloWorldBean helloWorldBean(){
+        return new HelloWorldBean("Hello World");
     }
 }
--- src/test/java/com/example/restfulwebservice/controller/HelloWorldControllerTest.java
+++ src/test/java/com/example/restfulwebservice/controller/HelloWorldControllerTest.java
@@ -9,8 +9,7 @@ import org.springframework.test.web.servlet.ResultActions;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -28,5 +27,15 @@ class HelloWorldControllerTest {
                 .andExpect(content().string("Hello World"))
                 .andDo(print())
         ;
+    }
+
+    @Test
+    public void Hello_World_Bean_test() throws Exception{
+        ResultActions action = mockMvc.perform(get("/hello-world-bean"));
+
+        action
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("message").value("Hello World"))
+                .andDo(print());
     }
 }
