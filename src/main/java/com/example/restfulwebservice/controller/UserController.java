8041e92c78c14a47e23ac2571d055c29b9f7959a
Dongwoo Yang <ckck803@gmail.com>
2020-12-23 16:36:46 +0900

    사용자 목록 조회 & 등록을 위한 Api 구현

---
+++ src/main/java/com/example/restfulwebservice/controller/UserController.java
@@ -0,0 +1,30 @@
+package com.example.restfulwebservice.controller;
+
+import com.example.restfulwebservice.domain.User;
+import com.example.restfulwebservice.repository.UserDaoService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserDaoService service;
+
+    @GetMapping("/users")
+    public List<User> retrieveAllUsers(){
+        return service.findAll();
+    }
+
+    @GetMapping("/users/{id}")
+    public User retrieveUser(@PathVariable int id){
+        return service.findOne(id);
+    }
+
+    @PostMapping("/users")
+    public void createUser(@RequestBody User user){
+        User savedUser = service.save(user);
+    }
+}
---
+++ src/test/java/com/example/restfulwebservice/controller/UserControllerTest.java
@@ -0,0 +1,67 @@
+package com.example.restfulwebservice.controller;
+
+import com.example.restfulwebservice.domain.User;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import java.util.Date;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class UserControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Test
+    public void 모든_유저들을_가져온다() throws Exception {
+        ResultActions resultActions = mockMvc.perform(get("/users"));
+
+        resultActions
+                .andExpect(status().isOk())
+                .andDo(print());
+    }
+
+    @Test
+    public void 특정_유저를_가져온다() throws Exception {
+        ResultActions resultActions = mockMvc.perform(get("/users/1"));
+
+        resultActions
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("id").value(1))
+                .andDo(print());
+    }
+
+    @Test
+    public void 유저를_등록한다() throws Exception {
+        User user = new User();
+        String name = "dongwoo";
+        Date date = new Date();
+
+        user.setName(name);
+        user.setJoinDate(date);
+        String content = objectMapper.writeValueAsString(user);
+
+        ResultActions resultActions = mockMvc.perform(post("/users")
+                .content(content)
+                .contentType(MediaType.APPLICATION_JSON));
+
+        resultActions
+                .andExpect(status().isOk())
+                .andDo(print());
+    }
+}
